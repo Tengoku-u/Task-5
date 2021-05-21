23da5e5ad716c72e6b6111d4b8b8a504344a42a6
Oye <80988722+Tengoku-u@users.noreply.github.com>
2021-05-21 09:25:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Task 5
+ javaScript Task
---
+++ sc.js
@@ -0,0 +1,50 @@
+function convertFahrToCelsius(num) {
+    let str = JSON.stringify(num);
+    let result = (num - 32) * (5/9);
+    
+     if (typeof num === "number") {
+        console.log(result.toFixed(4));
+        return result.toFixed(4);   
+    }  
+    else if (Array. isArray(num)) {
+        console.log(str + " is not a valid number but an array")
+    } else {
+        console.log(str + " is not a valid number but a/an "  + typeof num);
+    }
+    return result;
+}
+convertFahrToCelsius(9);
+
+
+
+
+function yuGiOhReplacer(num) {
+    let yuGiOh = [];
+    let str = JSON.stringify(num);
+    if (typeof num === "number") {
+
+        for (let i =1; i<=num; i++) {
+            let tempNum = "";
+            if (i % 2 === 0) {
+                tempNum += (tempNum === "")? "yu" : "-yu";
+            }
+            if (i % 3 === 0) {
+                tempNum += (tempNum === "")? "gi" : "-gi";
+            }
+            if (i % 5 === 0) {
+                tempNum += (tempNum === "")? "oh" : "-oh";
+            }
+            if (tempNum === "") {
+                tempNum =i;
+            }
+            yuGiOh.push(tempNum);  
+        }
+    } 
+     
+    if (typeof num !== "number") {
+        console.log("invalid parameter: " + str);
+    }
+    console.log(yuGiOh);
+    return yuGiOh;
+}
+yuGiOhReplacer(50);
